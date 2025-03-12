b8830808d297cd3d5a8c7a2af650fb868caa1318
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 16:48:24 +0700

    menambahkan class KodeProduk

---
+++ KodeProduk.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+internal class KodeProduk
+{
+		public enum elektronik // enum elekttronik di gunakan untuk menyimpan daftar elektronik
+		{
+			Laptop,
+			Smarthphone,
+			Tablet,
+			Headset,
+			Keyboard,
+			Mouse,
+			Printer,
+			Monitor,
+			Smartwatch,
+			Kamera
+		}
+
+    // Setiap indeks dari enum elektronik digunakan untuk mengakses array KodeElektronikList, yang berisi kode pos untuk masing-masing elektronik sesuai urutan.
+    private static String[] KodeElektronikList = { "E100", "E101", "E102", "E103", "E104", "E105", "E106", "E107", "E108", "E109"};
+
+    // Untuk mendapatkan kode pos berdasarkan elektronik, digunakan method GetKodeElektronik(elektronik Elektronik), yang mengambil nilai kode produk dari array
+    // berdasarkan indeks enum.
+
+    public static String GetKodeElektronik(elektronik Elektronik)
+		{
+			return KodeElektronikList((int)Elektronik];
+		}
+}
--- jurnalMod4_103022300072/Program.cs
+++ jurnalMod4_103022300072/Program.cs
@@ -1,4 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-//inisial
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // menampilkan daftar kelurahan beserta kode posnya dengan memanggil  KodeProduk.GetKodeElektronik untuk setiap kelurahan yang terdaftar dalam enum Kelurahan.
+        Console.WriteLine("Produk : " + KodeProduk.GetKodeElektronik(KodeProduk.elektronik.Laptop));
+    }
+}
